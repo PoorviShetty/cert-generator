b519445755824ab4992cefefa3899b99569bdf6c
Alaap Surendran <alaapsurendran@gmail.com>
2021-10-12 16:28:15 +0530

    Added a smol flask webapp ui thing
    Basically to know whats happening :')

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
+
 import cairosvg
 import csv
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 from flask_mail import Mail, Message
+import time
 import os
 
 from dotenv import load_dotenv
 load_dotenv('.env')
 
 app = Flask(__name__)
+executor = ThreadPoolExecutor(max_workers = 10)
+app.config['SECRET_KEY'] = os.getenv('APP_SECRET')
+sockio = SocketIO(app)
 
 EMAIL_USERNAME = os.getenv('EMAIL_USERNAME')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
@@ -35,49 +41,47 @@ class Template:
 
 def export_and_send(template, name, email):
     with app.app_context():
-        print("[{}] Worker Thread started".format(email))
+        time.sleep(1)
+        sockio.emit("status", {"email": email, "status":"🟧 Running"})
         cairosvg.svg2pdf(bytestring=template.to_str({"name":name}), write_to="./out/cert_{}.pdf".format(email))
         print("[{}] Finished generating certificate".format(email))
-
-        # todo: Figure out how to send email
-        # send email to user
-        msg = Message(
-            'Congratulations',
-            sender=('GDSC JSSSTU', EMAIL_USERNAME),
-            recipients=[email]
-        )
-        #msg.html = render_template('email.html')
-        msg.body = 'Here is your certificate!'
-        with app.open_resource("out/cert_{}.pdf".format(email)) as fp:
-            msg.attach("out/cert_{}.pdf".format(email), 'application/pdf', fp.read())
-        mail.send(msg)
-
+        try:
+            msg = Message(
+                'Congratulations',
+                sender=('GDSC JSSSTU', EMAIL_USERNAME),
+                recipients=[email]
+            )
+            #msg.html = render_template('email.html')
+            msg.body = 'Here is your certificate!'
+            with app.open_resource("out/cert_{}.pdf".format(email)) as fp:
+                msg.attach("out/cert_{}.pdf".format(email), 'application/pdf', fp.read())
+            mail.send(msg)
+            sockio.emit("status", {"email": email, "status":"🟩 Done"})
+        except Exception as e:
+            sockio.emit("status", {"email": email, "status":"🟥 Failed"})
+            print(e)
         return email
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    
-    temp = Template('./template/android.svg')
+    temp = Template('./cert_template/android.svg')
     list_file = open('list.csv', 'r')
     list_reader = csv.reader(list_file)
     next(list_reader) # header
     
-    futures = set()
+    name_email_list = []
 
-    with ThreadPoolExecutor(max_workers = 5) as executor:
-        for line in list_reader:
-            name = line[0].strip()
-            email = line[1].strip()
-            futures.add(executor.submit(export_and_send, temp, name, email))
-        for future in as_completed(futures):
-            print("[main] Finished sending email to {}".format(future.result()))
+    for line in list_reader:
+        name = line[0].strip()
+        email = line[1].strip()
+        name_email_list.append({"name" : name, "email" : email})
+        executor.submit(export_and_send, temp, name, email)
     list_file.close()
-
-    return "Messages Sent"
-
+    return render_template("index.html", name_email = name_email_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    sockio.run(app, debug=True)
 
 
     
